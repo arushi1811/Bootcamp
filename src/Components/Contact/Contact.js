e66115abf4c91be839680a4ba5db543209483106
arushi1811 <arushig@bu.edu>
2021-05-24 22:31:45 -0400

    first commit

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -11,9 +11,9 @@ function Contact() {
         <Container fluid>
           <h1 className="display-3">My Contact Details</h1>
           <p className="lead">Here are my contact details </p>
-          <p className="lead">Email : sachin@wynisco.com </p>
-          <p className="lead">Phone : 732 874 0188 </p>
-          <p className="lead">Typical Wake up / Sleep Hours : Other than bootcamp. I will available morning 7AM to 11AM EST via slack or whatsapp  </p>
+          <p className="lead">Email : arushig@bu.edu </p>
+          <p className="lead">Phone : 617 834 4710 </p>
+          <p className="lead">Typical Wake up / Sleep Hours : Other than bootcamp. I will available morning 11AM to 3AM EST via slack or whatsapp  </p>
         </Container>
       </Jumbotron>
     </div>
